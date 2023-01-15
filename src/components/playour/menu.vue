9c85cbd713eaafdd895c7bbfe382c46373817697
binhantt <doan44503@gmail.com>
2023-01-15 21:21:58 +0700

    shop an

---
+++ src/components/playour/menu.vue
@@ -0,0 +1,63 @@
+<template>
+    <body>
+	<div class="meta">
+		<a href="" class="meta-1">
+			<a href=""class="meta-1-1"style="color: black;">
+				<img src="https://static-znews.zingcdn.me/images/logo-zing-home.svg">
+				<span>Tạp chí tri thức trực tuyến</span>
+			</a>
+			<ul class="meta-menu"style="list-style-type:none; !important;">
+				<li class="meta-menu-xs"><a href="" >xuất sứ
+				</a></li>
+				<li class="meta-menu-kd" ><a href="" > kinh doanh</a></li>
+				<li class="meta-menu-cg" ><a href="" >công nghệ</a></li>
+				<li class="meta-menu-tt" ><a href="">thể thao</a></li>
+				<li class="meta-menu-gt"><a href="">giải trí</a></li>
+				<li class="meta-menu-ds"><a href="">đời sống</a></li>
+				<li class="meta-menu-Lifestyle" ><a href="">Lifestyle</a></li>
+			</ul>
+		</a>
+
+	</div>
+</body>
+</template>
+<style>
+    body{
+		background-color: white;
+		margin: 0;
+	}
+ .meta-1-1{
+ 	flex-direction: column-reverse;
+ 	align-items: baseline;
+ 	display: flex;
+ 	width: 150px;
+ 	height: 30px;
+ 	font-size: 10px;
+ 	font-family: Arial, Helvetica, sans-serif;
+ 	justify-content: flex-end;
+ 	text-decoration: none;
+	align-items: center;
+	}
+.meta-1-1 img {
+	width: 101px;
+	height: 103px;
+}
+.meta-menu{
+	display: flex;
+	font-size: 15px;
+	background-color: white ;
+	}
+.meta{
+	display: flex;
+	background: 
+}
+.meta-menu li a:hover{
+ color: ;
+ 
+ }
+</style>
+<script>
+export default{
+    name: "nemu"
+}
+</script>
---
+++ src/views/trangchu.vue
@@ -0,0 +1,15 @@
+
+  <script setup >
+  import navbar from '../components/layout/menu.vue';
+  import footers from '../components/layout/footers.vue';
+ 
+  </script>
+  <template>
+      <navbar/>
+    <footers/>
+ </template>
+ <style>
+ html{
+  margin:0;
+ }
+</style>
